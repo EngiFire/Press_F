e785d8aef50b0688daed0793ee0fb0580867122c
Алексей Волощенко <voloshchenko.alexey01@gmail.com>
2022-07-17 17:14:28 +0300

    V_0.5

--- summer_practice/src/App.jsx
+++ summer_practice/src/App.jsx
@@ -37,32 +37,51 @@
 // }
 
 import { Routes, Route, Link } from 'react-router-dom';
+import { Container, Navbar, NavbarBrand, Nav} from 'react-bootstrap';
 
 import {Home} from './Pages/Home';
 import {Stories} from './Pages/Stories';
 import {Account} from './Pages/Account';
 import { Notfound } from './Pages/Notfound';
 
+import logo from './Components/logo1.png';
+
 function App() {
   return (
     <>
-      <div>
-        ffffffffffffff!!
+    <div class='wrapper'>
+      <div id="rectangle"></div>
+      <div class="header">
+        <NavbarBrand href='/welcome'>
+          <img class='logo'
+            src={logo}
+            alt='Logo' 
+          />
+        </NavbarBrand>
+          {/* <div class="burger-menu">
+              <div class="nav-burger"><a href="#about">О салоне</a></div>
+              <div class="nav-burger">Цены</div>
+              <div class="nav-burger">Контакты</div>
+            </div> */}
+        <div class="nav-menu">
+            <div class="nav-item"><Link to='/welcome'>Home</Link></div>
+            <div class="nav-item"><Link to='/stories'>Stories</Link></div>
+            <div class="nav-item"><Link to='/account'>Account</Link></div>
+            {/* <div class="nav-open">
+                <img style="width: 30px" src="./menu.png" alt="" srcset=""  draggable="false" />
+            </div> */}
+        </div>
       </div>
-      <header>
-        <Link to='/'>Home</Link>
-        <Link to='/stories'>Stories</Link>
-        <Link to='/account'>Account</Link>
-      </header>
-      <div>
-        Hiiii!!
+
+      <div class='content'>
+        <Routes>
+          <Route path='/welcome' element={<Home />} />
+          <Route path='/stories/' element={<Stories />} />
+          <Route path='/account/' element={<Account />} />
+          <Route path='/*' element={<Notfound />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/stories/' element={<Stories />} />
-        <Route path='/account/' element={<Account />} />
-        <Route path='/*' element={<Notfound />} />
-      </Routes>
+    </div>
     </>
   );
 }
--- summer_practice/src/Components/Header.jsx
+++ summer_practice/src/Components/Header.jsx
@@ -1,31 +1,22 @@
-import React, { Component } from 'react';
-import { Container, Navbar, NavbarBrand} from 'react-bootstrap';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Container, Navbar, NavbarBrand, Nav} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from './logo1.png';
 
-function Header () {
-    return (
-        <>
-        <Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
-            <Container>
-            <NavbarBrand href='/'>
-                <img
-                src={logo}
-                height='60'
-                width='60'
-                alt='Logo' 
-                />
-            </NavbarBrand>
+function BreadcrumbExample() {
+  return (
+    <Navbar fixed='top' collapseOnSelect expand='md' bg='dark' variant='dark'>
             <Breadcrumb>
-                <Breadcrumb.Item href="/welcome"><Link to='/'>Home</Link></Breadcrumb.Item>
-                <Breadcrumb.Item href="/stories"><Link to='/stories'>Stories</Link></Breadcrumb.Item>
-                <Breadcrumb.Item href="/account"><Link to='/account'>Account</Link></Breadcrumb.Item>
+                <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+                <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                    Library
+                </Breadcrumb.Item>
+                <Breadcrumb.Item active><Link to='/account'>Account</Link></Breadcrumb.Item>
             </Breadcrumb>
-            </Container>
-        </Navbar>
-        </>
-    );
+        <div>
+            Hello!!!!!!!!!!
+        </div>
+    </Navbar>
+  );
 }
 
-export default Header;
+export default BreadcrumbExample;
